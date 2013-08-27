bac392aada3f98a99633be056fdbee6764681c28
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-08-27 17:39:16 +0300

    SB build 13.1.20131.2469

--- EN/HtmlSamples/map/bing-maps/demo.js
+++ EN/HtmlSamples/map/bing-maps/demo.js
@@ -8,7 +8,7 @@ $(function () {
                 verticalZoomable: true,
                 horizontalZoomable: true,
                 overviewPlusDetailPaneVisibility: "visible",
-                overviewPlusDetailPaneBackgroundImageUri: "http://igniteuisamples.dev.infragistics.local/13-1/images/samples/maps/world.png",
+                overviewPlusDetailPaneBackgroundImageUri: "http://dev.igniteui.local/13-1/images/samples/maps/world.png",
                 panModifier: "control",
                 backgroundContent: {
                     type: "bing",
@@ -19,8 +19,8 @@ $(function () {
                     name: "worldCountries",
                     markerType: "automatic",
                     shapeMemberPath: "points",
-                    shapefileUri: "http://igniteuisamples.dev.infragistics.local/13-1/data-files/shapes/world.shp",
-                    databaseUri: "http://igniteuisamples.dev.infragistics.local/13-1/data-files/shapes/world.dbf"
+                    shapefileUri: "http://dev.igniteui.local/13-1/data-files/shapes/world.shp",
+                    databaseUri: "http://dev.igniteui.local/13-1/data-files/shapes/world.dbf"
                 }],
                 windowResponse: "immediate",
                 windowRect: {
